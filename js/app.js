0075fad98e3dd9d7c4dcbc742aa5295902d97903
remrain <cy@remrain.com>
2017-01-07 15:22:23 +0800

    init

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 angular.module('app', ['ngSanitize', 'ng-showdown', 'ngResource']);
 angular.module('app').controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.text = '#hello, markdown!';
-    console.log('inited');
+    $scope.text = '';
+    $scope.editing = true;
+    $scope.doEdit = function () {
+        $scope.editing = true;
+    };
+    $scope.doPreview = function () {
+        $scope.editing = false;
+    };
     $http.get('data/sample.md').then(function (res) {
         $scope.text = res.data;
     })
